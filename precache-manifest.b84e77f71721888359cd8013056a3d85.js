e27f65eb5c8a7cbc4cc4cbf4be7aabc74909c08c
Ryan Joo <rhjoo@Ryans-Mac-mini.local>
2019-03-04 19:56:37 -0800

    Updates

---
+++ precache-manifest.b84e77f71721888359cd8013056a3d85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "3dd0e094ca0f39cab3c1",
+    "url": "/todo-react/static/js/main.3dd0e094.chunk.js"
+  },
+  {
+    "revision": "f13764b5f062601a8467",
+    "url": "/todo-react/static/js/1.f13764b5.chunk.js"
+  },
+  {
+    "revision": "3dd0e094ca0f39cab3c1",
+    "url": "/todo-react/static/css/main.a6b336f0.chunk.css"
+  },
+  {
+    "revision": "370e8b95e9efb74574413eb7b39ec7fb",
+    "url": "/todo-react/index.html"
+  }
+];
